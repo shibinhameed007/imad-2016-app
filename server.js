c91b19a7a5cb8e08ca59acc7291ec847906e571c
shibinhameed007 <shibinhameed007@gmail.com>
2016-10-01 12:47:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,86 +5,106 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    
-    title: 'article One | Shibin' ,
-    heading: 'Article-One',
-    date: 'sep 9,2016' ,
-    content: `
-    <p>
-            this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
-        </p>
-        <p>
-        this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
-        </p>
-        <p>
-            this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
-        </p>
-        `
-    
-};
+var articles={
+'article-One' : {
 
+title: 'article One | Shibin' ,
+heading: 'Article-One',
+date: 'sep 9,2016' ,
+content: `
+<p>
+        this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
+    </p>
+    <p>
+    this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
+    </p>
+    <p>
+        this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
+    </p>
+    `
+
+},
+'article-Two' :  {
+
+title: 'article Two | Shibin' ,
+heading: 'Article-Two',
+date: 'sep 19,2016' ,
+content: `
+<p>
+        this is the content for my second article.
+    </p>
+    `
+
+},
+'article-Three' :  {
+
+title: 'article Three | Shibin' ,
+heading: 'Article-One',
+date: 'sep 39,2016' ,
+content: `
+<p>
+        this is the content for my third article. 
+
+    </p>
+    `
+
+},
+};
 function createTemplate(data){
- var date=data.date;
- var heading= data.heading;
- var title= data.title;
- var content= data.content;
+var date=data.date;
+var heading= data.heading;
+var title= data.title;
+var content= data.content;
 
 var htmlTemplate =
-    `<html>
+`<html>
 <head>
-    <title>
-    ${title}
-        </title>
- <link href="/ui/style.css" rel="stylesheet" />
+<title>
+${title}
+    </title>
+<link href="/ui/style.css" rel="stylesheet" />
 </head>
 
 <body>
- <div class="container">
-  <div>
-        <a href="/">Home </a>
-    </div>
-    <hr/>
-    <h3> 
-    ${heading}
-    </h3>
-    <div>
-        ${date}
-    </div>  
-    <div>
-       ${content}
+<div class="container">
+<div>
+    <a href="/">Home </a>
 </div>
-  </div>
+<hr/>
+<h3> 
+${heading}
+</h3>
+<div>
+    ${date}
+</div>  
+<div>
+   ${content}
+</div>
+</div>
 </body>
 </html>` ;
 
 return htmlTemplate;
 }
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+res.send(createTemplate(articleOne));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+res.send(createTemplate(articles[articleName]));
+var articleName= req.parans.articleName;
 });
 
-app.get('/article-two', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(`IMAD course app listening on port ${port}!`);
 });
