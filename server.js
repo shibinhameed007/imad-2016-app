025c1ea53c66eca20a877c2b4489c586fb0857f1
shibinhameed007 <shibinhameed007@gmail.com>
2016-10-01 13:40:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-res.sendFile(_dirname,path.join('ui','index.html'));
+res.sendFile(__dirname,path.join('ui','index.html'));
 });
 
 var articles={
