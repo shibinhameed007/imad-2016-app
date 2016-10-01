16d2736e9951694020b03312f03c5e0bdd2f5fbc
shibinhameed007 <shibinhameed007@gmail.com>
2016-10-01 13:34:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,10 +86,10 @@ ${heading}
 
 return htmlTemplate;
 }
-app.get('/', function (req, res) {
+//app.get('/', function (req, res) {
 res.sendFile(path.join('ui','index.html'));
 });
-
+//
 app.get('/:articleName', function (req, res) {
 res.send(createTemplate(articles[articleName]));
 var articleName= req.parans.articleName;
