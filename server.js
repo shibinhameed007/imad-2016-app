68eaef124d05aff5bda377fee524dd2b9e55c4a6
shibinhameed007 <shibinhameed007@gmail.com>
2016-10-01 13:47:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,10 @@ var articleName= req.parans.articleName;
 });
 
 app.get('/ui/style.css', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/main.js', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
