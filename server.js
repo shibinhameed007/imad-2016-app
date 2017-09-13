5720cff566ee9febb1be745623cf2b617ba4b2af
shibinhameed007 <shibinhameed007@gmail.com>
2017-09-13 17:57:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,112 +5,103 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname,'ui','index.html'));
-});
-
-var articles={
-'article-One' : {
-
-title: 'article One | Shibin' ,
-heading: 'Article-One',
-date: 'sep 9,2016' ,
-content: `
-<p>
-        this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
-    </p>
-    <p>
-    this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
-    </p>
-    <p>
-        this is the content for my first article. this is the content for my first article. this is the content for my first article. this is the content for my first article.
-    </p>
-    `
-
-},
-'article-Two' :  {
-
-title: 'article Two | Shibin' ,
-heading: 'Article-Two',
-date: 'sep 19,2016' ,
-content: `
-<p>
-        this is the content for my second article.
-    </p>
-    `
-
-},
-'article-Three' :  {
-
-title: 'article Three | Shibin' ,
-heading: 'Article-One',
-date: 'sep 30,2016' ,
-content: `
-<p>
-        this is the content for my third article. 
-
-    </p>
-    `
-
-},
+var articleOne = {
+    title : 'Article-one | Shibin',
+    date  : 'Sep 9th 2017',
+    heading : 'Article-One',
+    content : 
+            `<p>
+                This is the first article.    This is the first article.    This is the first article.    This is the first article.
+                    This is the first article.    This is the first article.
+                        This is the first article.
+            </p>
+            <p>
+                This is the first article.    This is the first article.    This is the first article.    This is the first article.
+                    This is the first article.    This is the first article.
+                        This is the first article.
+            </p>
+            <p>
+                This is the first article.    This is the first article.    This is the first article.    This is the first article.
+                    This is the first article.    This is the first article.
+                        This is the first article.
+            </p>`
+    
+    
 };
-function createTemplate(data){
-var date=data.date;
-var heading= data.heading;
-var title= data.title;
-var content= data.content;
 
-var htmlTemplate =
-`<html>
+function createTemplate (data) {
+    var title= data.title
+    var heading= data.heading
+    var date= data.date
+    var content= data.content
+
+var htmlTemplate=`
+    
+    <html>
+
 <head>
-<title>
-${title}
+    <title>
+        ${title}
+        
     </title>
-<link href="/ui/style.css" rel="stylesheet" />
+    <link href="/ui/style.css" rel="stylesheet" />
 </head>
+    <body>
+      <div class=container>
+             
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h2>
+            ${heading}
+        </h2>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}               
+        </div>
+      </div>
+    </body>
+    
 
-<body>
-<div class="container">
-<div>
-    <a href="/">Home </a>
-</div>
-<hr/>
-<h3> 
-${heading}
-</h3>
-<div>
-    ${date}
-</div>  
-<div>
-   ${content}
-</div>
-</div>
-</body>
-</html>` ;
 
+
+</html>
+`;
 return htmlTemplate;
 }
 
-
-app.get('/:articleName', function (req, res) {
-res.send(createTemplate(articles[articleName]));
-var articleName= req.parans.articleName;
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.send(createTemplate(articleOne));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('article three 3 req and will be served here');
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-console.log(`IMAD course app listening on port ${port}!`);
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
 });
